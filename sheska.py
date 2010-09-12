252d826272d44a8b422586d902d5fe6a4d5fa391
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-09-12 00:14:44 -0700

    switch to calling and parsing output of exiftool

--- sheska.py
+++ sheska.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
 
-from libxmp import XMPFiles, XMPMeta, object_to_dict
-from libxmp.consts import XMP_NS_DC
+from subprocess import Popen, PIPE
 
 class File(object):
 	def __init__(self, filename):
-		self.xmpfile = XMPFiles(file_path=filename, open_forupdate=True)
-		self.xmp = self.xmpfile.get_xmp() or XMPMeta()
-		self._generate_taglist()
+		self.filename = filename
+		unparsedTagLine = self.call('-xmp:TagsList')
+		if unparsedTagLine=='':
+			self.tags = []
+		else:
+			self.tags = [tag.strip() for tag in unparsedTagLine.split(':')[1].split(',')]
 	
-	def _generate_taglist(self):
-		tags = []
-		dict = object_to_dict(self.xmp)
-		
-		if dict.has_key(XMP_NS_DC): # there may not be any Dublin Core metadata
-			for entry in dict[XMP_NS_DC]:
-				if entry[0].startswith('dc:subject['):
-					tags.append(entry[1])
-		
-		self.tags = tags
+	def __str__(self):
+		return "%s: %s" % (self.filename, self.tags)
+	
+	call = lambda self, arg: Popen(['exiftool', arg, self.filename], stdout=PIPE).communicate()[0]
 	
 	def write_taglist(self):
+		# If you aren't using digiKam, you probably want to use the Dublin Core
+		# 'Subject' attribute.  That would work here, too, but adding to TagsList
+		# adds the appropriate tags in digiKam even if the file's already in the
+		# library, which doesn't happen with Subject.
+		
+		# first, erase what previously existed in the file
+		self.call('-xmp:TagsList=')
+		
+		# and then write out all of our current tags
 		for tag in self.tags:
-			self.xmp.append_array_item(XMP_NS_DC, 'subject', tag)
-	
-	def close(self):
-		self.xmpfile.put_xmp(self.xmp)
-		self.xmpfile.close_file()
+			self.call('-xmp:TagsList+="%s"' % tag)
