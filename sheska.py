a8bf90cd8147dcf505d0177e9fc594e945d3735a
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-09-12 02:16:17 -0700

    now in business

--- sheska.py
+++ sheska.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
+import json
+import sys
+from hashlib import md5
 from subprocess import Popen, PIPE
+from urllib2 import urlopen, URLError
 
 class File(object):
 	def __init__(self, filename):
 		self.filename = filename
-		unparsedTagLine = self.call('-xmp:TagsList')
-		if unparsedTagLine=='':
-			self.tags = []
+		self.tags = []
+		
+		# since adding and removing tags changes the md5 hash,
+		# we'll store the original hash in the metadata
+		print "Checking for original hash... ",
+		unparsedTagLine = self.call('-xmp:md5sum')
+		if unparsedTagLine:
+			self.hash = unparsedTagLine.split(':')[1].strip()
 		else:
-			self.tags = [tag.strip() for tag in unparsedTagLine.split(':')[1].split(',')]
+			print "hashing... ",
+			self.hash = md5(file(filename, 'r').read()).hexdigest()
+			self.call('-xmp:md5sum=%s' % self.hash)
+		print "ok"
 	
 	def __str__(self):
 		return "%s: %s" % (self.filename, self.tags)
@@ -27,4 +39,29 @@ class File(object):
 		
 		# and then write out all of our current tags
 		for tag in self.tags:
-			self.call('-xmp:TagsList+="%s"' % tag)
+			self.call('-xmp:TagsList+=%s' % tag)
+
+if __name__ == '__main__':
+	for filename in sys.argv[1:]:
+		print "Processing %s" % filename
+		image = File(filename)
+		
+		print "Fetching tags from the internets..."
+		# we'll just assume that the request will eventually get through
+		while True:
+			try:
+				# %3A is a colon (':'), encoded for urls
+				unparsedJson = urlopen('http://chan.sankakucomplex.com/post/index.json?tags=md5%3A' + image.hash)
+				break
+			except URLError:
+				print "Request timed out."
+		parsedJson = json.load(unparsedJson)
+		if not parsedJson:
+			print "FILE_NOT_FOUND\n"
+			continue
+		
+		# split string on spaces and replace underscores with spaces
+		image.tags = [tag.replace('_', ' ') for tag in parsedJson[0]['tags'].split(' ')]
+		print "Writing new tags..."
+		image.write_taglist()
+		print "Done!\n"
